ee963950c0f7a2d7419029acb4b972248178def8
elyssahancock <elyssabh@byui.edu>
2023-08-22 23:04:40 -0600

    pricing updates

--- views/pricing.php
+++ views/pricing.php
@@ -21,7 +21,7 @@
         <!-- <h1 class="heading">Pricing</h1> -->
         <section class="heading">
             <div class="image-container">
-                <img src="https://placekitten.com/700/200" alt="placeholder photo">
+                <img src="https://placeholder.com/700/200" alt="placeholder photo">
                 <div class="image-text">Pricing</div>
             </div>
     
@@ -30,7 +30,7 @@
     
         <!-- ---------------- ENGAGEMENT PRICING SECTION ---------------- -->
         <section class="engagements">
-            <img class="centered" src="https://placekitten.com/160/288" alt="placeholder">
+            <img id="engagement-image" src="https://placeholder.com/160/288" alt="placeholder">
     
             <h2 class="pricing-section-title">Engagements</h2>
     
@@ -52,27 +52,33 @@
     
         <!-- ---------------- WEDDING 1 PRICING SECTION ---------------- -->
         <section class="wedding1">
+            <img id="wedding1-image" src="https://placeholder.com/160/288" alt="placeholder">
+
             <h2 class="pricing-section-title">Wedding Package A</h2>
+
+            
+            <p class="pricing-paragraphs">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Eaque non quia expedita quam laborum! Sequi vitae temporibus provident nisi cupiditate, quasi in ex,
+                velit et eveniet corrupti a, repellendus laborum. Lorem ipsum dolor sit, amet consectetur adipisicing
+                elit.
+                Harum nihil ratione maiores laborum iusto explicabo error alias architecto. Nesciunt perferendis
+                blanditiis
+                eaque,
+                consectetur tempora molestiae consequuntur expli</p>
+            
+            <hr class="pricing-packages-divider">
             <h2 class="pricing-amount">$700</h2>
-            <p class="pricing-paragraphs">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Eaque non quia expedita quam laborum! Sequi vitae temporibus provident nisi cupiditate, quasi in ex,
-                velit et eveniet corrupti a, repellendus laborum. Lorem ipsum dolor sit, amet consectetur adipisicing
-                elit.
-                Harum nihil ratione maiores laborum iusto explicabo error alias architecto. Nesciunt perferendis
-                blanditiis
-                eaque,
-                consectetur tempora molestiae consequuntur expli</p>
-            <img class="centered" src="https://placekitten.com/160/288" alt="placeholder">
-    
     
     
         </section>
     
     
-        <!-- ---------------- ENGAGEMENT PRICING SECTION ---------------- -->
+        <!-- ---------------- WEDDING PACKAGE B PRICING SECTION ---------------- -->
         <section class="engagements">
-            <h2 class="pricing-section-title">Engagements</h2>
-            <h2 class="pricing-amount">$200</h2>
+        <img id="wedding2-image" src="https://placeholder.com/160/288" alt="placeholder">
+
+            <h2 class="pricing-section-title">Wedding Package B</h2>
+            
             <p class="pricing-paragraphs">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Eaque non quia expedita quam laborum! Sequi vitae temporibus provident nisi cupiditate, quasi in ex,
                 velit et eveniet corrupti a, repellendus laborum. Lorem ipsum dolor sit, amet consectetur adipisicing
@@ -81,14 +87,14 @@
                 blanditiis
                 eaque,
                 consectetur tempora molestiae consequuntur expli</p>
-            <img class="centered" src="https://placekitten.com/160/288" alt="placeholder">
-    
-    
+            
+
+            <hr class="pricing-packages-divider">
+            <h2 class="pricing-amount">$1,000</h2>
     
         </section>
     
-        <img src="https://placekitten.com/300/250" alt="placeholder photo" class="centered">
-    </main>
+        
     <footer id="main-footer">
         <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/MadisonGrantPhoto/snippets/footer.php"?>
     </footer>
